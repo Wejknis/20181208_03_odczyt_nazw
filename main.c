0034c12a687ad3cfbfa654b96e040684559df728
Wejknis <weja077@gmail.com>
2018-12-08 10:31:23 +0100

    1 2 3 start jeszcze coś

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int liczba_slow, char* tablica_slow[]) {
+
+    int i;
+
+    for (i = 0; i < liczba_slow; ++i){
+        printf("parametr %i: %s\n", i, tablica_slow[i]);
+    }
+
     return 0;
 }
